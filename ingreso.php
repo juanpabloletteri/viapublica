069ea5e6db5b9ae20c2e9f54531929ce58b4fc17
juanpabloletteri <pabloletteri@gmail.com>
2017-05-13 02:15:31 -0300

    base ingreso

---
+++ ingreso.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- icono en la parte superior -->
+    <link rel="icon" href="./dist/favicon.ico">
+    <!-- sweet alert -->
+    <script src="./dist/sweetalert-dev.js"></script>
+    <link rel="stylesheet" href="./dist/sweetalert.css">
+    <!-- bootstrap y jquery -->
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    
+    <title>INGRESO</title>
+</head>
+<body>
+    
+</body>
+</html>
